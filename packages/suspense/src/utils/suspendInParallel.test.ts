37ca53bb24c8e46a407e6cdb3fc867a5ba80339d
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-02-22 20:05:43 -0500

    Add suspendInParallel util

---
+++ packages/suspense/src/utils/suspendInParallel.test.ts
@@ -0,0 +1,29 @@
+import { suspendInParallel } from "./suspendInParallel";
+
+describe("suspendInParallel", () => {
+  it("should call all callbacks before re-throwing any thrown value", () => {
+    const callbackA = jest.fn();
+    const callbackB = jest.fn().mockImplementation(() => {
+      throw Error("Expected error");
+    });
+    const callbackC = jest.fn();
+
+    expect(() => {
+      suspendInParallel(callbackA, callbackB, callbackC);
+    }).toThrow("Expected error");
+
+    expect(callbackA).toHaveBeenCalled();
+    expect(callbackB).toHaveBeenCalled();
+    expect(callbackC).toHaveBeenCalled();
+  });
+
+  it("should return all values if none of the callbacks suspend", () => {
+    const callbackA = () => 123;
+    const callbackB = () => "abc";
+
+    const [a, b] = suspendInParallel(callbackA, callbackB);
+
+    expect(a).toEqual(123);
+    expect(b).toEqual("abc");
+  });
+});
--- packages/suspense/src/utils/suspendInParallel.ts
+++ packages/suspense/src/utils/suspendInParallel.ts
@@ -1,3 +1,27 @@
-export function suspendInParallel() {
-  // TODO
+type AnyFunction<ReturnType> = (...args: any[]) => ReturnType;
+
+// Helper function to read from multiple Suspense caches in parallel.
+// This method will re-throw any thrown value, but only after also calling subsequent caches.
+export function suspendInParallel<T extends AnyFunction<any>[]>(
+  ...callbacks: [...T]
+): { [K in keyof T]: ReturnType<Extract<T[K], AnyFunction<any>>> } {
+  const values: any[] = [];
+
+  let thrownValue = null;
+
+  callbacks.forEach((callback) => {
+    try {
+      values.push(callback());
+    } catch (error) {
+      thrownValue = error;
+    }
+  });
+
+  if (thrownValue !== null) {
+    throw thrownValue;
+  }
+
+  return values as {
+    [K in keyof T]: ReturnType<Extract<T[K], AnyFunction<any>>>;
+  };
 }
