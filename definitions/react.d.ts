748ed6449f410dcea2fa1e947a4a663636bd83cb
Maciek Sakrejda <m.sakrejda@gmail.com>
2024-12-07 13:21:31 -0800

    Builds but fails useCacheMutation tests

--- definitions/react.d.ts
+++ definitions/react.d.ts
@@ -2,9 +2,6 @@ import "react";
 
 declare module "react" {
   // Unstable APIs used by this package ...
-
-  export function unstable_getCacheForType<T>(resourceType: () => T): T;
-
   export function unstable_useCacheRefresh(): (
     resourceType?: () => any,
     seed?: any
